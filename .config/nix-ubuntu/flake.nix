4891a159eaa7fb0d4917d50567de82c810149ff2
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2024-12-02 12:06:51 +0100

    Ubuntu flake changing.

--- .config/nix-ubuntu/flake.nix
+++ .config/nix-ubuntu/flake.nix
@@ -1,61 +1,31 @@
 {
-  description = "Ubuntu system flake";
+  description = "My Ubuntu Nix";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = inputs@{ self, nixpkgs }: 
-  let
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    # system = "aarch64-linux"; If you are running on ARM powered computer
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    
-    commonPackages = with pkgs; [
-      neovim
-      ffmpeg
-      git
-      git-lfs
-      stow
-      starship
-      tmux
-      rar
-      ripgrep
-      python313
-      scons
-      cmake
-      tree
-      wget
-      curl
-      htop
-    ];
-  in
-  {
-    packages.${system}.default = pkgs.buildEnv {
-      name = "ubuntu-environment";
-      paths = commonPackages;
-    };
-
-    nixosConfigurations.ubuntu = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ({ config, pkgs, ... }: {
-          nixpkgs.config.allowUnfree = true;
-          
-          nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-          environment.systemPackages = commonPackages;
-
-          system.stateVersion = "23.11";
-
-          users.users.your-username = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-            shell = pkgs.bash;
-          };
-        })
-      ];
+    # myuser = builtins.getEnv "USER";
+    myuser = "stuart-warren";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations = {
+      "${myuser}" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home-manager/home.nix
+        ];
+      };
     };
   };
 }
